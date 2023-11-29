9ba58be59428e8a44bc54d82054ee377e9c4a08f
razzytirta <azzi.fachrurazzi@gmail.com>
2023-11-30 05:27:38 +0800

    Dependency, Import, Compile, Upgrade Package

---
+++ bin/belajar_dart_application.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library_2023/hello.dart' as belajar;
+
+void main() {
+  print(belajar.sayHello('Fachrurazzi'));
+
+  var customer = belajar.Customer();
+  var product = belajar.Product();
+  var category = belajar.Category();
+}
